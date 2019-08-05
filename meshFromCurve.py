c5ab2e0ca6ed9160e930805e8d6a0a5a1cf02d0d
Scthe <marcin1113C@gmail.com>
2019-08-05 21:00:49 +0200

    Update for Blender 2.80

--- meshFromCurve.py
+++ meshFromCurve.py
@@ -18,226 +18,288 @@
 
 bl_info = {
     "name": "Mesh From Curve",
-    "author": "Scthe",
-    "version": (0, 1),
-    "blender": (2, 72, 0),
-    "location": "View3D > Tools -> Create",
+    "author": "Marcin Matusczyk (Scthe)",
+    "version": (2, 0, 0),
+    "blender": (2, 80, 0),
+    "location": "View3D > Add > Mesh > Mesh From Curve",
     "description": "Create Mesh based on curve",
     "warning": "",
     "wiki_url": "",
     "category": "Tools"}
-    
+
 import bpy
-from bpy.utils import register_module, unregister_module
-from bpy.props import IntProperty
-from bpy.props import BoolProperty
+from bpy.types import Operator
+from bpy.props import IntProperty, BoolProperty
 import cmath
+import logging
+
+log = logging.getLogger(__name__)
 
 
-''' add edge to the mesh '''
-def add_edge( mesh, idA, idB):
-	mesh.edges.add(1)
-	mesh.edges[-1].vertices[0] = idA
-	mesh.edges[-1].vertices[1] = idB
 
-def create_mesh( center, vertices, nSegments, add_faces, cyclic, nCurves):
-	# base object creation
-	mesh = bpy.data.meshes.new("my_mesh")
-	obj = bpy.data.objects.new('mfc', mesh)
-	bpy.context.scene.objects.link(obj)
-	obj.show_wire = True
+#####################
+### Create final object
 
-	obj.location = center			   # location
-	for vID in range( len(vertices) ):
-		#print ( "v: "+str(vertices[vID]))
-		for i in range(3):			  # translation from global to local system
-			vertices[vID][i] -= center[i]
-			
-	#print( "creating mesh: add faces-"+str(add_faces))
-	faces = []
-	if not add_faces:   # create edges
-		for vID in range(1, len(vertices)):
-			if vID % nSegments == 0:
-				continue
-			add_edge( mesh, vID - 1, vID)
-	else:			  # add faces	
-		for i in range(nSegments -1): # go all the way around
-			for j in range(nCurves - 1):
-				base = j * nSegments + i
-				if (base + 1) % nSegments == 0: # do not connect first and last
-					continue
-				f =  ( base,\
-					base + 1,\
-					base + nSegments + 1,\
-					base + nSegments )
-				faces.append(f)
-		if cyclic:
-			pass
+def set_object_center(obj, vertices, center):
+    obj.location = center
+    # translation from global to local system
+    for vID in range(len(vertices)):
+        for i in range(3):
+            vertices[vID][i] -= center[i]
 
-	mesh.from_pydata( vertices, [], faces)
+def create_mesh_edges_only(mesh, vertices, segment_count):
+    edges = []
+    for vID in range(1, len(vertices)):
+        if vID % segment_count == 0:
+            continue
+        edges.append([vID - 1, vID])
+    return edges
 
-''' p1, p2 <- arrays of 3 values [x, y, z] '''
-def distance( p1, p2):
-	#return cmath.sqrt(sum( [ (a-b)**2 for a,b in zip(p1, p2) ] ))
-	L = [ p1[0] - p2[0] , p1[1] - p2[1] , p1[2] - p2[2] ]
-	return abs( cmath.sqrt( L[0]*L[0] + L[1]*L[1] + L[2]*L[2]) )
+def create_mesh_with_faces(vertices, curves_count, segment_count):
+    def create_face_indices(baseIdx):
+        return (baseIdx,
+                baseIdx + 1,
+                baseIdx + segment_count + 1,
+                baseIdx + segment_count)
 
-''' point between start and end in range of 'dist' from start '''
-def getPoint ( start, end, dist ):
-	delta = [0, 0, 0]
-	l = [0, 0, 0]
-	percent = dist / distance(start, end)
+    faces = []
+    for i in range(segment_count - 1): # go all the way around
+        for j in range(curves_count - 1):
+            base = j * segment_count + i
+            if (base + 1) % segment_count == 0: # do not connect first and last
+                continue
+            face = create_face_indices(base)
+            faces.append(face)
+    return faces
 
-	for i in range (3):
-		delta[i] = end[i] - start[i]
-		l[i] = start[i] + (percent *delta[i])
-	return l
+def create_object(context, center, vertices, curves_count, segment_count):
+    context = context or bpy.context
+    scene = context.scene
+    layer = context.view_layer
+    layer_collection = context.layer_collection or layer.active_layer_collection
+    scene_collection = layer_collection.collection
 
-''' returns obj's vertices in expected order and in global coordinates system '''
-def getSplineVertices( obj):
-	wmtx = obj.matrix_world 
-	result = []
-	for v in obj.data.vertices:
-		act = [ e.real for e in (wmtx * v.co)]
-		result.append(act)
-	return result
+    mesh = bpy.data.meshes.new("mesh_from_curve_mesh")
+    obj = bpy.data.objects.new('mesh_from_curve_object', mesh)
+    scene.collection.objects.link(obj)   # add the data to the scene as an object
+    obj.show_wire = True
+    set_object_center(obj, vertices, center)
 
-''' create mesh'ed version of curve ( assuming that curve is already selected)'''
-def create_dummy_object():
-	bpy.ops.object.convert( target = 'MESH', keep_original = True )
-	obj = bpy.context.object
-	
-	bpy.ops.object.transform_apply( location = False, rotation = False, scale = True)
-	# print("created : " + obj.name)
-	return obj
+    if curves_count == 1:
+        edges = create_mesh_edges_only(mesh, vertices, segment_count)
+        faces = []
+    else:
+        edges = []
+        faces = create_mesh_with_faces(vertices, curves_count, segment_count)
 
-''' deselect all objects TODO: merge with select_only '''
+    mesh.from_pydata(vertices, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+
+    obj.select_set(True)
+    layer.update() # apply location
+    layer.objects.active = obj
+
+
+#####################
+### Utils
+
+def distance(p1, p2):
+    ''' :p1, :p2 <- arrays of 3 values [x, y, z] '''
+    #return cmath.sqrt(sum( [ (a-b)**2 for a,b in zip(p1, p2) ] ))
+    L = [ p1[0] - p2[0] , p1[1] - p2[1] , p1[2] - p2[2] ]
+    return abs( cmath.sqrt( L[0]*L[0] + L[1]*L[1] + L[2]*L[2]) )
+
 def deselect_all():
-	for o in  bpy.data.objects:
-		o.select = False
+    ''' deselect all objects '''
+    bpy.ops.object.select_all(action='DESELECT')
 
-''' select one object '''
-def select_only( obj):
-	bpy.context.scene.objects.active = obj
-	obj.select = False
-	obj.select = True
+def select_only(obj):
+    ''' select one object '''
+    deselect_all()
+    obj.select_set(True)
 
-''' create points -> calculate theirs locations '''
-def fill_between( org_points, segment_length, nSegments):
-	list = []
-	segmentID = 0				  # id of last used point from org_points collection
-	active_point = org_points[0]	# last added point
-	active_segment_len = 0.0001		# overhead from last loop iteration
+def report_error(operator, msg):
+    if operator is not None:
+        operator.report({'ERROR_INVALID_CONTEXT'}, msg)
+    log.error('[ERROR] ' + msg)
 
-	for i in range( nSegments - 2):
-		next_point_in = segment_length  # distance to next point
-		active_segment_len = distance(active_point, org_points[ segmentID + 1])
-		while next_point_in > active_segment_len: #and segmentID + 2 < len(SPLINE_VERTICES):
-			segmentID += 1 # move to the next segment
-			active_point = org_points[segmentID]
-			next_point_in -= active_segment_len
-			active_segment_len = distance(active_point, org_points[ segmentID + 1])
-		p = getPoint(active_point, org_points[segmentID + 1], next_point_in)
-		active_point = p
-		list.append( p )
-	return list
+def get_world_position(obj, vert_idx):
+    wmtx = obj.matrix_world
+    co = obj.data.vertices[vert_idx].co
+    return [e.real for e in (wmtx @ co)]
 
-''' main algorithm '''
-def curveMesh( nSEGMENTS, cyclic = False):
 
-	#print("\n--------------\n")
-	New_Vertices_List = []
-	Object_Center = [ 0, 0, 0] # list, so we can add this objects
-	
-	selected_Curves = [ o for o in bpy.data.objects if o.select and o.type == "CURVE"]
-	deselect_all()
-	
-	for curve in selected_Curves:
-		#print('CURVE:' + curve.name)
-		
-		for i in range(3):
-			Object_Center[i] += curve.location[i]
-		select_only( curve)
-	   
-		# create mesh from this curve to iterate easier between points object
-		dummy = create_dummy_object()
-		curve.select = False
-		select_only(dummy)
-	
-		# Get total length
-		total_length = 0
-		for vID in range( len (dummy.data.vertices) -1 ): # TODO optimize !
-			total_length += distance( dummy.data.vertices[vID].co,\
-									  dummy.data.vertices[vID + 1].co )
-		if cyclic: total_length += distance( dummy.data.vertices[ 0].co,\
-									  dummy.data.vertices[-1].co )
-		segment_length = total_length /( nSEGMENTS -1)
+#####################
+### Calculate positions of vertices - main part of this script
 
-		spline_vertices = getSplineVertices( dummy)
-		if cyclic:
-			wmtx = dummy.matrix_world 
-			act = [ e.real for e in (wmtx * dummy.data.vertices[0].co)]
-			spline_vertices.append(act)
+def get_curve_vertices(obj):
+    ''' returns :obj's vertices in expected order and in global coordinates system '''
+    vert_cnt = len(obj.data.vertices)
+    return [get_world_position(obj, vert_id) for vert_id in range(vert_cnt)]
 
-		# after this, spline_vertices is already in global coordinates system
+def get_point_between(start, end, dist):
+    ''' point between :start and :end in range of :dist from start '''
+    l = [0, 0, 0]
+    percent = dist / distance(start, end)
 
-		# algorithm
-		New_Vertices_List.append(spline_vertices[0])
-		middle_vertices = fill_between( spline_vertices, segment_length, nSEGMENTS)
-		New_Vertices_List.extend(middle_vertices)
-		New_Vertices_List.append(spline_vertices[-1]) # !!!
-		
-		# cleaning
-		bpy.ops.object.delete()
-		#print()
-	
-	# end
-	Object_Center = [ c / len(selected_Curves) for c in Object_Center] 
-	create_mesh( center = Object_Center, vertices = New_Vertices_List, nSegments = nSEGMENTS,\
-		add_faces = len(selected_Curves) > 1, cyclic = cyclic, nCurves = len(selected_Curves))
+    for i in range (3):
+        delta = end[i] - start[i]
+        l[i] = start[i] + (percent * delta)
+    return l
 
-	
+def calculate_vertices_positions_on_curve(curve_points, segment_length, segment_count):
+    result = [curve_points[0]] # add start
+    last_original_vertex = 0 # id of last used point from original curve_points collection
+    measure_start_point = curve_points[0] # coordinates from which we start measurement
 
-class MeshFromCurve(bpy.types.Operator):
-	bl_idname = "mesh.meshfromcurve"
-	bl_label = "Mesh From Curve"
-	bl_description = "Create Mesh based on curve"
-	bl_options = {"REGISTER", "UNDO"}
+    for i in range(segment_count - 2):
+        next_point_in = segment_length  # distance to next point
+        to_next_orginal_vertex  = distance(
+            measure_start_point,
+            curve_points[last_original_vertex + 1]
+        )
 
-	segments = IntProperty(name="Segments", description="Segment count", default = 50, min = 2, max = 1000)
-	#invert = BoolProperty(name="Invert order", description="Try only when everything has failed", default=False) # TODO invert order
-	cyclic = BoolProperty(name="Cyclic", description="Is the shape closed ?", default=False)
+        # move along the curve till we find segment into which we will add vertex
+        while next_point_in > to_next_orginal_vertex:
+            last_original_vertex += 1
+            measure_start_point = curve_points[last_original_vertex]
+            next_point_in -= to_next_orginal_vertex
+            to_next_orginal_vertex  = distance(
+                measure_start_point,
+                curve_points[last_original_vertex + 1]
+            )
 
-	@classmethod
-	def poll(cls,context):
-		return(context.mode == "OBJECT")
+        # get vertex position
+        p = get_point_between(
+            measure_start_point, curve_points[last_original_vertex + 1],
+            next_point_in
+        )
+        measure_start_point = p
+        result.append(p)
 
-	def invoke(self,context, event):
-		return self.execute(context)
+    result.append(curve_points[-1]) # add end
+    return result
 
-	def execute(self,context):
-		curveMesh(self.segments + 1, self.cyclic)
-		return {"FINISHED"}
+def get_curve_length(curve_mesh, cyclic):
+    verts = curve_mesh.data.vertices
+    total_length = 0
 
-def menu_draw (self,context):
-	self.layout.operator_context = "INVOKE_REGION_WIN"
-	self.layout.operator(MeshFromCurve.bl_idname,
+    for vID in range(len(verts) - 1):
+        total_length += distance(verts[vID].co, verts[vID + 1].co)
+
+    if cyclic:
+        total_length += distance(verts[0].co, verts[-1].co)
+    return total_length
+
+def all_selected_are_curves():
+    selection = bpy.context.selected_objects
+    all_curves = all(o.type == "CURVE" for o in selection)
+    return len(selection) > 0 and all_curves
+
+def create_object_from_curves(context, operator, segment_count, cyclic):
+    ''' main algorithm '''
+    if not all_selected_are_curves():
+        report_error(operator, 'Only curves can be selected')
+        return False
+
+    # curves can be interpolated. I do not want to write/search for interpolation
+    # methods. Convert curves to smooth meshes and use meshes as basis instead
+    bpy.ops.object.convert(target = 'MESH', keep_original = True)
+    selected_curves = bpy.context.selected_objects
+
+    result_vertices = []
+    center = [0, 0, 0]
+
+    for curve in selected_curves:
+        select_only(curve)
+        bpy.ops.object.transform_apply(location = False, rotation = False, scale = True)
+
+        # get curve props
+        total_length = get_curve_length(curve, cyclic)
+        segment_length = total_length / (segment_count - 1)
+        spline_vertices = get_curve_vertices(curve) # in world coordinates system
+        if cyclic:
+            starting_vert = get_world_position(curve, 0)
+            spline_vertices.append(starting_vert)
+
+        # algorithm
+        verts = calculate_vertices_positions_on_curve(spline_vertices, segment_length, segment_count)
+        result_vertices.extend(verts)
+
+        for i in range(3):
+            center[i] += curve.location[i]
+
+        # cleaning
+        bpy.ops.object.delete()
+
+    # end
+    center = [c / len(selected_curves) for c in center]
+    create_object(
+        context,
+        center,
+        result_vertices,
+        len(selected_curves), segment_count
+    )
+    return True
+
+
+
+#####################
+### Blender addon
+
+class OBJECT_OT_mesh_from_curve(Operator):
+    ''' Create Mesh based on curve '''
+    bl_idname = "mesh.mesh_from_curve"
+    bl_label = "Mesh From Curve"
+    bl_description = "Create Mesh based on curve"
+    bl_options = {"REGISTER", "UNDO"}
+
+    segments: IntProperty(
+        name="Segments",
+        description="Segment count",
+        default = 50,
+        min = 2,
+        max = 1000
+    )
+    cyclic: BoolProperty(
+        name="Cyclic",
+        description="Close the shape",
+        default=False
+    )
+
+    @classmethod
+    def poll(cls, context):
+        return context.mode == "OBJECT"
+
+    def invoke(self, context, event):
+        return self.execute(context)
+
+    def execute(self, context):
+        ok = create_object_from_curves(
+            context, self,
+            self.segments + 1, self.cyclic
+        )
+        if not ok:
+            return {"CANCELLED"}
+        return {"FINISHED"}
+
+def menu_draw (self, context):
+    self.layout.operator_context = "INVOKE_REGION_WIN"
+    self.layout.operator(
+        OBJECT_OT_mesh_from_curve.bl_idname,
         text="Mesh From Curve",
         icon='CURVE_DATA')
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_mesh_add.append(menu_draw)
-    bpy.types.VIEW3D_PT_tools_add_object.append(menu_draw)
- 
+    bpy.utils.register_class(OBJECT_OT_mesh_from_curve)
+    bpy.types.VIEW3D_MT_mesh_add.append(menu_draw)
+
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_mesh_add.remove(menu_draw)
-    bpy.types.VIEW3D_PT_tools_add_object.remove(menu_draw)
-    
+    bpy.utils.unregister_class(OBJECT_OT_mesh_from_curve)
+    bpy.types.VIEW3D_MT_mesh_add.remove(menu_draw)
 
 if __name__ == "__main__":
-	register()
-	#curveMesh( 95,True)
-	#print('fin')
-	
+    # unregister()
+    register()
+    # create_object_from_curves(None, None, 95, True)
